3bf46f737d0912b5b982e846792322561ba57d77
andreportol <andreportol@gmail.com>
2023-01-19 14:19:31 -0400

    Atualização do template index, criação das rotas url e configuração da views.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import atualizar, cadastrar, deletar, index, pesquisar
 
 urlpatterns = [
     path('', index, name='index'),
+    path('cadastrar', cadastrar, name='cadastrar'),
+    path('deletar', deletar, name='deletar'),
+    path('atualizar', atualizar, name='atualizar'),
+    path('pesquisar', pesquisar, name='pesquisar')
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def cadastrar(request):
+    return render(request, 'cadastrar.html')
+
+def deletar(request):
+    return render(request, 'deletar.html')
+
+def atualizar(request):
+    return render(request, 'atualizar.html')
+
+def pesquisar(request):
+    return render(request, 'pesquisar.html')
