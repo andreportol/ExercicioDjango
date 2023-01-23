af626379cca35c6337ce52457456fe1e68fd3d86
andreportol <andreportol@gmail.com>
2023-01-23 13:18:32 -0400

    Criado o decorator admin.register para apresentar as informações na página admin do django.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cadastro
+
+
+@admin.register(Cadastro)
+class CadastroAdmin(admin.ModelAdmin):
+    list_display = ('Inscricao_Imobiliaria', 'Bairro','Regiao Urbana')
+
