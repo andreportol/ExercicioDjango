253dcb606294727dc6b394f7d08c5b02cf0fa342
andreportol <andreportol@gmail.com>
2023-01-23 17:04:39 -0400

    Configurando os arquivos para enviar mais dados ao banco de dados

--- core/forms.py
+++ core/forms.py
@@ -6,5 +6,7 @@ from .models import Cadastro
 class CadastroModelForm(forms.ModelForm):
    class Meta:
       model = Cadastro
-      fields = ['insc_imob']
+      fields = ['insc_imob','utm_x', 'utm_y','reg_urbana', 'bairro_imovel', 'parcelamento_imovel',
+      'quadra_imovel', 'lote_imovel', 'logradouro_imovel', 'numero_imovel', 'obs_imovel']   
+      
 
--- core/models.py
+++ core/models.py
@@ -14,7 +14,17 @@ class Base(models.Model):
 
 class Cadastro(Base):
     insc_imob = models.CharField('Inscricao',max_length=11)
-       
+    utm_x = models.DecimalField('UTM_X', max_digits=6, decimal_places=3)
+    utm_y = models.DecimalField('UTM_Y', max_digits=6, decimal_places=3) 
+    reg_urbana = models.CharField('Região Urbana', max_length=50)  
+    bairro_imovel = models.CharField('Bairro', max_length=100)
+    parcelamento_imovel = models.CharField('Parcelamento', max_length=5)
+    quadra_imovel = models.CharField('Quadra', max_length=5)
+    lote_imovel = models.CharField('Lote', max_length=3)
+    logradouro_imovel = models.CharField('Inscricao',max_length=120)
+    numero_imovel = models.CharField('Inscricao',max_length=7)
+    obs_imovel = models.TextField('Observacao')
+    
     def __str__(self):
         return f'{self.insc_imob}'
 
